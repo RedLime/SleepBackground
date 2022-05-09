dee86acc1c0a5f72f339670c647eca0cec4e25ed
RedLime <redlimerl@gmail.com>
2022-05-09 21:28:51 +0900

    fix input delay

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
@@ -5,23 +5,21 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MinecraftClient.class)
 public abstract class MixinMinecraftClient {
 
-    @Inject(method = "getFramerateLimit", at = @At("HEAD"), cancellable = true)
-    public void onFocused(CallbackInfoReturnable<Integer> cir) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        Window window = client.getWindow();
+    @Shadow protected abstract int getFramerateLimit();
 
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getFramerateLimit()I"))
+    public int onFocused(MinecraftClient client) {
+        Window window = client.getWindow();
         boolean isHovered = (GLFW.glfwGetWindowAttrib(window.getHandle(), 131083) != 0);
 
-        if (!client.isWindowFocused() && !isHovered) {
-            cir.setReturnValue(SleepBackground.BG_FRAME_RATE);
-        }
+        return (!client.isWindowFocused() && !isHovered) ? SleepBackground.BG_FRAME_RATE : this.getFramerateLimit();
     }
 
 }
