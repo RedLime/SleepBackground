b74012520b6e95585eac644fa8cc84c9d745828b
RedLime <redlimerl@gmail.com>
2022-05-10 17:07:08 +0900

    change fps limit method

--- src/main/java/com/redlimerl/sleepbackground/SleepBackground.java
+++ src/main/java/com/redlimerl/sleepbackground/SleepBackground.java
@@ -2,10 +2,16 @@ package com.redlimerl.sleepbackground;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Window;
+import net.minecraft.util.Util;
 import org.apache.commons.io.FileUtils;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.locks.LockSupport;
 
 public class SleepBackground implements ClientModInitializer {
 
@@ -23,5 +29,49 @@ public class SleepBackground implements ClientModInitializer {
         } catch (Throwable e) {
             e.printStackTrace();
         }
+    }
+
+    private static long lastRenderTime = 0;
+    public static boolean shouldRenderInBackground() {
+        long currentTime = Util.getMeasuringTimeMs();
+        long timeSinceLastRender = currentTime - lastRenderTime;
+
+        if (!shouldRenderInBackground(timeSinceLastRender))
+            return false;
+
+        lastRenderTime = currentTime;
+        return true;
+    }
+
+    private static boolean shouldRenderInBackground(long timeSinceLastRender) {
+        Integer fpsOverride = getBackgroundFPS();
+        if (fpsOverride == null) {
+            return true;
+        }
+
+        long frameTime = (1000 / fpsOverride);
+        boolean shouldSkipRender = (timeSinceLastRender < frameTime);
+        if (!shouldSkipRender)
+            return true;
+
+        idle(frameTime);
+        return false;
+    }
+
+    private static void idle(long waitMillis) {
+        waitMillis = Math.min(waitMillis, 30L);
+        LockSupport.parkNanos("waiting to render", waitMillis * 1000000L);
+    }
+
+    @Nullable
+    private static Integer getBackgroundFPS() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        return (!client.isWindowFocused() && !isHoveredWindow())
+         ? BG_FRAME_RATE : null;
+    }
+
+    private static boolean isHoveredWindow() {
+        Window window = MinecraftClient.getInstance().getWindow();
+        return GLFW.glfwGetWindowAttrib(window.getHandle(), 131083) != 0;
     }
 }
--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
@@ -2,24 +2,52 @@ package com.redlimerl.sleepbackground.mixin;
 
 import com.redlimerl.sleepbackground.SleepBackground;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.Window;
-import org.lwjgl.glfw.GLFW;
+import net.minecraft.client.gui.screen.LevelLoadingScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.world.ClientWorld;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public abstract class MixinMinecraftClient {
 
-    @Shadow protected abstract int getFramerateLimit();
+    @Shadow @Nullable public ClientWorld world;
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;getFramerateLimit()I"))
-    public int onFocused(MinecraftClient client) {
-        Window window = client.getWindow();
-        boolean isHovered = (GLFW.glfwGetWindowAttrib(window.getHandle(), 131083) != 0);
+    @Shadow @Nullable public Screen currentScreen;
 
-        return (!client.isWindowFocused() && !isHovered) ? SleepBackground.BG_FRAME_RATE : this.getFramerateLimit();
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundManager;updateListenerPosition(Lnet/minecraft/client/render/Camera;)V"), cancellable = true)
+    public void onInput(CallbackInfo ci) {
+        if (!SleepBackground.shouldRenderInBackground()
+            && (this.world != null || this.currentScreen instanceof LevelLoadingScreen)) {
+            ci.cancel();
+        }
     }
+
+/*
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;render(FJZ)V"))
+    public void onFocused(GameRenderer instance, float tickDelta, long startTime, boolean tick) {
+        if (SleepBackground.shouldRenderInBackground()) {
+            instance.render(tickDelta, startTime, tick);
+        } else {
+            SleepBackground.SHOULD_CHECK_INPUT = true;
+        }
+    }
+
+    @Inject(method = "handleInputEvents", at = @At("HEAD"), cancellable = true)
+    public void onInput(CallbackInfo ci) {
+        if (SleepBackground.SHOULD_CHECK_INPUT) {
+            ci.cancel();
+            SleepBackground.SHOULD_CHECK_INPUT = false;
+        }
+    }
+
+    @Inject(method = "tick", at = @At("RETURN"))
+    public void onTick(CallbackInfo ci) {
+        System.out.println("tick" + System.currentTimeMillis());
+    }*/
 
 }
