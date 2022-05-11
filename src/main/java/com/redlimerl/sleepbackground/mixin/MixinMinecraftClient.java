3c56e15f496ba0934b063b9476f8a87c1a8e2967
RedLime <redlimerl@gmail.com>
2022-05-11 15:10:44 +0900

    fix fast window update

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
@@ -6,6 +6,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.profiler.Profiler;
 import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,6 +23,7 @@ public abstract class MixinMinecraftClient {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundManager;updateListenerPosition(Lnet/minecraft/client/render/Camera;)V"), cancellable = true)
     public void onRender(CallbackInfo ci) {
         if (!SleepBackground.shouldRenderInBackground()) {
+            GLFW.glfwPollEvents();
             this.profiler.pop();
             ci.cancel();
         }
