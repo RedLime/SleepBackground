5105b8cbf40ef47d88689c8233693ba4e6013cc5
RedLime <redlimerl@gmail.com>
2022-05-18 11:28:03 +0900

    fix pie chart

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
@@ -4,9 +4,11 @@ import com.redlimerl.sleepbackground.SleepBackground;
 import com.redlimerl.sleepbackground.config.ConfigValues;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.MetricsData;
 import net.minecraft.util.profiler.Profiler;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,11 +22,16 @@ public abstract class MixinMinecraftClient {
 
     @Shadow public abstract Profiler getProfiler();
 
+    @Shadow @Final public MetricsData metricsData;
+
+    @Shadow private long lastMetricsSampleTime;
+
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundManager;updateListenerPosition(Lnet/minecraft/client/render/Camera;)V", shift = At.Shift.AFTER), cancellable = true)
     public void onRender(CallbackInfo ci) {
         if (!SleepBackground.shouldRenderInBackground()) {
             GLFW.glfwPollEvents();
             this.getProfiler().pop();
+            skipAllProfiler();
             ci.cancel();
         }
         SleepBackground.checkRenderWorldPreview();
@@ -35,4 +42,12 @@ public abstract class MixinMinecraftClient {
         SleepBackground.CLIENT_WORLD_TICK_COUNT = this.world == null ? 0 :
                 Math.min(SleepBackground.CLIENT_WORLD_TICK_COUNT + 1, ConfigValues.WORLD_INITIAL_FRAME_RATE.getMaxTicks());
     }
+
+    @SuppressWarnings("ALL")
+    private void skipAllProfiler() {
+        long m = System.nanoTime();
+        this.metricsData.pushSample(m - this.lastMetricsSampleTime);
+        this.lastMetricsSampleTime = m;
+    }
+
 }
