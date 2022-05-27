7791bc46218e4de4ad202f853fc2444447c75b53
pixfumy <asaddurrani1@live.ca>
2022-05-27 01:13:41 -0400

    port to 1.8

--- src/main/java/com/redlimerl/sleepbackground/mixin/GameRendererMixin.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/GameRendererMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "method_9775", at = @At("HEAD"), cancellable = true)
     private void onRender(CallbackInfo ci) {
         if (!SleepBackground.shouldRenderInBackground()) {
             ci.cancel();
