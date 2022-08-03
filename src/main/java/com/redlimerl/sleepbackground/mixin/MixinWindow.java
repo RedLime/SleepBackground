51e09a0c82227dba326783bddda349af9292160a
RedLime <redlimerl@gmail.com>
2022-08-03 18:21:11 +0900

    change version name

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinWindow.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinWindow.java
@@ -2,6 +2,7 @@ package com.redlimerl.sleepbackground.mixin;
 
 import com.redlimerl.sleepbackground.SleepBackground;
 import net.minecraft.class_4117;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,6 +15,7 @@ public class MixinWindow {
     public void onSwap(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) {
             ci.cancel();
+            GLFW.glfwPollEvents();
         }
     }
 }
