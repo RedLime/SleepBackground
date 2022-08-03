5283e434ab130a66553738ae84cae56ba46adbb1
RedLime <redlimerl@gmail.com>
2022-08-03 18:49:39 +0900

    Revert "3.1"
    This reverts commit a2d9c67aa4c53db4e036635a6c79e5ab857097fc.

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinWindow.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinWindow.java
@@ -2,20 +2,18 @@ package com.redlimerl.sleepbackground.mixin;
 
 import com.redlimerl.sleepbackground.SleepBackground;
 import net.minecraft.client.util.Window;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Window.class)
-public abstract class MixinWindow {
+public class MixinWindow {
 
     @Inject(method = "swapBuffers", cancellable = true, at = @At("HEAD"))
     public void onSwap(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) {
             ci.cancel();
-            GLFW.glfwPollEvents();
         }
     }
 }
