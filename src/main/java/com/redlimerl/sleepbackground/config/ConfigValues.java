ccf9162f70ed6b1101995ed356b878dc89f2f151
RedLime <redlimerl@gmail.com>
2022-08-09 17:44:31 +0900

    3.3

--- src/main/java/com/redlimerl/sleepbackground/config/ConfigValues.java
+++ src/main/java/com/redlimerl/sleepbackground/config/ConfigValues.java
@@ -15,6 +15,9 @@ public class ConfigValues {
     public static final FrameLimitConfigValue NONE_PLAYING_FRAME_RATE =
             new FrameLimitConfigValue("lock_instance", 1, "It works when instance is in the background with sleepbg.lock file is exist in user directory (for macros option)");
 
+    public static final TickIntervalConfigValue NONE_PLAYING_TICK_INTERVAL =
+            new TickIntervalConfigValue("lock_instance_tick", 1, "It checks does sleepbg.lock file exists at every X ticks.");
+
     public static final FrameLimitConfigValue LOADING_SCREEN_FRAME_RATE =
             new FrameLimitConfigValue("loading_screen", 30, "It works when instance is in the world loading screen. minimum (fps_limit) is 15.") {
                 @Override
@@ -40,5 +43,6 @@ public class ConfigValues {
         ALL_CONFIGS.add(WORLD_PREVIEW_RENDER_TIMES);
         ALL_CONFIGS.add(WORLD_INITIAL_FRAME_RATE);
         ALL_CONFIGS.add(NONE_PLAYING_FRAME_RATE);
+        ALL_CONFIGS.add(NONE_PLAYING_TICK_INTERVAL);
     }
 }
