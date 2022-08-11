f23b8feea74a257a44b7b157595b0165d887fcca
RedLime <redlimerl@gmail.com>
2022-08-11 17:16:18 +0900

    fix mapping

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinRenderSystem.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinRenderSystem.java
@@ -12,27 +12,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GL11.class)
 public class MixinRenderSystem {
 
-    @Inject(method = "glClear", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "glClear", at = @At("HEAD"), cancellable = true, remap = false)
     private static void onClear(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
 
-    @Inject(method = "glPushMatrix", at = @At("HEAD"), cancellable = true, expect = 0, require = 0)
+    @Inject(method = "glPushMatrix", at = @At("HEAD"), cancellable = true, remap = false, expect = 0, require = 0)
     private static void onPush(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
 
-    @Inject(method = "glPopMatrix", at = @At("HEAD"), cancellable = true, expect = 0, require = 0)
+    @Inject(method = "glPopMatrix", at = @At("HEAD"), cancellable = true, remap = false, expect = 0, require = 0)
     private static void onPop(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
 
-    @Inject(method = "glEnable", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "glEnable", at = @At("HEAD"), cancellable = true, remap = false)
     private static void onCull(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
 
-    @Inject(method = "glFlush", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "glFlush", at = @At("HEAD"), cancellable = true, remap = false)
     private static void onTexture(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
