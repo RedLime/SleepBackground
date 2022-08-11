8bccee3b6ba279d36e6c637c505d1479f65c9166
RedLime <redlimerl@gmail.com>
2022-08-11 17:09:25 +0900

    fix mapping

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinGameRenderer.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinGameRenderer.java
@@ -17,4 +17,9 @@ public class MixinGameRenderer {
         }
         SleepBackground.checkRenderWorldPreview();
     }
+
+    @Inject(method = "method_19073", at = @At("HEAD"), cancellable = true)
+    public void onRenderStream(CallbackInfo ci) {
+        if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
+    }
 }
