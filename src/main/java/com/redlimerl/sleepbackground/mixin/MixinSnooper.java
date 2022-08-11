c2dc76acdfdbee08296acc67b40e4b5abe3f9d14
RedLime <redlimerl@gmail.com>
2022-08-11 15:56:08 +0900

    revert, found another way lol

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinSnooper.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinSnooper.java
@@ -3,10 +3,12 @@ package com.redlimerl.sleepbackground.mixin;
 import com.redlimerl.sleepbackground.SleepBackground;
 import net.minecraft.util.snooper.Snooper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Pseudo
 @Mixin(Snooper.class)
 public class MixinSnooper {
 
