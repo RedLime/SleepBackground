aab8c6efa1be90d73cb68fbf15415d5987a76d91
RedLime <redlimerl@gmail.com>
2022-08-15 13:32:52 +0900

    3.6

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinMinecraftClient.java
@@ -4,15 +4,12 @@ import com.redlimerl.sleepbackground.SleepBackground;
 import com.redlimerl.sleepbackground.config.ConfigValues;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
-import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.io.File;
 
 @Mixin(MinecraftClient.class)
 public abstract class MixinMinecraftClient {
@@ -24,17 +21,12 @@ public abstract class MixinMinecraftClient {
         SleepBackground.LATEST_LOCK_FRAME = !SleepBackground.shouldRenderInBackground();
     }
 
-    private int lockCheck = 0;
-
     @Inject(method = "tick", at = @At("RETURN"))
     public void onTick(CallbackInfo ci) {
         SleepBackground.CLIENT_WORLD_TICK_COUNT = this.world == null ? 0 :
                 Math.min(SleepBackground.CLIENT_WORLD_TICK_COUNT + 1, ConfigValues.WORLD_INITIAL_FRAME_RATE.getMaxTicks());
 
-        if (++lockCheck >= ConfigValues.NONE_PLAYING_FRAME_RATE.getTickInterval()) {
-            SleepBackground.LOCK_FILE_EXIST = new File(FileUtils.getUserDirectory(), "sleepbg.lock").exists();
-            lockCheck = 0;
-        }
+        SleepBackground.checkLock();
     }
 
 
