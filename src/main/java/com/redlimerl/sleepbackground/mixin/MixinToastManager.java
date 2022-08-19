5eebda0238ed02a32eee80015dcf39e0e65fb4db
RedLime <redlimerl@gmail.com>
2022-08-19 13:41:31 +0900

    fix 1.15 problem 2

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinToastManager.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinToastManager.java
@@ -3,14 +3,16 @@ package com.redlimerl.sleepbackground.mixin;
 import com.redlimerl.sleepbackground.SleepBackground;
 import net.minecraft.client.toast.ToastManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Pseudo
 @Mixin(ToastManager.class)
 public class MixinToastManager {
 
-    @Inject(method = "draw", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "draw", at = @At("HEAD"), cancellable = true, expect = 0, require = 0)
     public void onDraw(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
