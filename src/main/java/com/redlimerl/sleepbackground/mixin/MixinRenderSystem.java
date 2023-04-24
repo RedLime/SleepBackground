4851b31bf1e0d1673389b2a9ac9e805bcd5aa28e
RedLime <redlimerl@gmail.com>
2023-04-24 23:27:20 +0900

    22w13oneblockatatime

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinRenderSystem.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinRenderSystem.java
@@ -12,27 +12,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(RenderSystem.class)
 public class MixinRenderSystem {
 
-    @Inject(method = "clear", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "clear", at = @At("HEAD"), cancellable = true, remap = false)
     private static void onClear(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
 
-    @Inject(method = "enableCull", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "enableCull", at = @At("HEAD"), cancellable = true, remap = false)
     private static void onCull(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
 
-    @Inject(method = "enableTexture", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "enableTexture", at = @At("HEAD"), cancellable = true, remap = false)
     private static void onTexture(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
 
-    @Inject(method = "fogDensity", at = @At("HEAD"), cancellable = true, expect = 0, require = 0)
-    private static void onFogDensity(CallbackInfo ci) {
-        if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
-    }
-
-    @Inject(method = "fogMode(Lcom/mojang/blaze3d/platform/GlStateManager$FogMode;)V", at = @At("HEAD"), cancellable = true, expect = 0, require = 0)
+    @Inject(method = "setShaderFogStart", at = @At("HEAD"), cancellable = true, expect = 0, require = 0, remap = false)
     private static void onFogMode(CallbackInfo ci) {
         if (SleepBackground.LATEST_LOCK_FRAME) ci.cancel();
     }
