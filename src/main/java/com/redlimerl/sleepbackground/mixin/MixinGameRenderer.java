02d29511b6a832b23dcd5e5fa4dcd182ba1ce9d4
tildejustin <spacepepper3.14@gmail.com>
2024-05-31 03:03:52 -0400

    1.21-pre1 support

--- src/main/java/com/redlimerl/sleepbackground/mixin/MixinGameRenderer.java
+++ src/main/java/com/redlimerl/sleepbackground/mixin/MixinGameRenderer.java
@@ -1,17 +1,18 @@
 package com.redlimerl.sleepbackground.mixin;
 
 import com.redlimerl.sleepbackground.SleepBackground;
-import net.minecraft.client.render.GameRenderer;
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.render.*;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
-
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private void onRender(float tickDelta, long startTime, boolean tick, CallbackInfo callbackInfo) {
+    @Dynamic
+    // class_9779 -> RenderTickCounter
+    @Inject(method = {"render", "Lnet/minecraft/class_757;method_3192(Lnet/minecraft/class_9779;Z)V"}, at = @At("HEAD"), require = 1, cancellable = true)
+    private void onRender(CallbackInfo callbackInfo) {
         if (SleepBackground.LATEST_LOCK_FRAME) {
             callbackInfo.cancel();
         }
